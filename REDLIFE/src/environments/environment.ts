38c14f38d478be2bb2590b87f5e9933e51b2e19d
ivanaugusto86 <iaaleksink@gmail.com>
2020-04-18 11:42:15 -0300

    instalacion de firebase 2

--- REDLIFE/src/environments/environment.ts
+++ REDLIFE/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:  {
+    apiKey: "AIzaSyAVvzgsOEBTyb5PkoIJlAy5pKW-YUOi-rc",
+    authDomain: "abelia.firebaseapp.com",
+    databaseURL: "https://abelia.firebaseio.com",
+    projectId: "abelia",
+    storageBucket: "abelia.appspot.com",
+    messagingSenderId: "1035460197891",
+    appId: "1:1035460197891:web:6514db5e27ca0ef5135836",
+    measurementId: "G-GR0YED1R27"
+  }
+  // Initialize Firebase
 };
 
 /*
