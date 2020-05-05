fc0e8518b4948a85de55fc6ed5abcaaab95d3f59
Andrea Belen Enricci Louzan <abenriccilouzan@gmail.comgit config --global user.name>
2020-05-05 19:30:34 -0300

    añadi la funcion recuperar contraseña con firebase

--- REDLIFE/src/app/services/auth.service.ts
+++ REDLIFE/src/app/services/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import { user} from '../shared/user.class';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+ 
   constructor() { }
+
   async login(user:user)
   { 
     return firebase.auth().signInWithEmailAndPassword(user.email, user.password)
@@ -16,8 +18,6 @@ export class AuthService {
   { 
     return firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
   }
-    async recuperarcontraseña(user:user)
-    {
-      return firebase.auth().sendPasswordResetEmail
-    }
-}
+async recuperarcontrasena(email:string){
+  return firebase.auth().sendPasswordResetEmail(email)
+}}
