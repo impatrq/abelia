3ff96988da1e7b4bb185e22e216dbe02f6346f21
Andrea Belen Enricci Louzan <abenriccilouzan@gmail.comgit config --global user.name>
2020-05-05 19:30:03 -0300

    cree la page recuperacion contraseña y se agrego a la app routing

--- REDLIFE/src/app/app-routing.module.ts
+++ REDLIFE/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'recuperacioncontrasena',
+    loadChildren: () => import('./recuperacioncontrasena/recuperacioncontrasena.module').then( m => m.RecuperacioncontrasenaPageModule)
+  },
 
 ];
 
