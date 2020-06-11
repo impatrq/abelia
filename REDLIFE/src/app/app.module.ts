0fd8f2af5e5fae2472705e3b4a8b76c644a08205
Andrea Belen Enricci Louzan <abenriccilouzan@gmail.comgit config --global user.name>
2020-06-11 14:43:23 -0300

    Merge branch 'feature/configurarangularfire' into develop

--- REDLIFE/src/app/app.module.ts
+++ REDLIFE/src/app/app.module.ts
@@ -13,17 +13,17 @@ import * as firebase from 'firebase';
 import { environment } from '../environments/environment';
 
 import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-
+import { AngularFirestoreModule, } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 firebase.initializeApp(environment.firebaseConfig);
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule, AngularFireAuthModule],
+    AngularFirestoreModule, AngularFireAuthModule,  FormsModule,
+    ReactiveFormsModule],
   providers: [
     StatusBar,
     SplashScreen,
