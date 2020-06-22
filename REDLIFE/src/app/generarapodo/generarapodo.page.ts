fe49de96d1632077370a5ac0f2f9ee717289aeba
Andrea Belen Enricci Louzan <abenriccilouzan@gmail.comgit config --global user.name>
2020-06-22 15:52:34 -0300

    traigo los datos de db

--- REDLIFE/src/app/generarapodo/generarapodo.page.ts
+++ REDLIFE/src/app/generarapodo/generarapodo.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from '../services/auth.service';
+import { user} from '../shared/user.class';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore'
+import { FirestoreService} from '../services/firestore/firestore.service';
 @Component({
   selector: 'app-generarapodo',
   templateUrl: './generarapodo.page.html',
   styleUrls: ['./generarapodo.page.scss'],
 })
 export class GenerarapodoPage implements OnInit {
-
-  constructor() { }
+usuario: user
+  constructor(private router: Router, private db: AngularFirestore,  private fb: FirestoreService) { }
 
   ngOnInit() {
   }
-
+ traercoleccion() {
+  this.fb.traercoleccion()
 }
+}
+
