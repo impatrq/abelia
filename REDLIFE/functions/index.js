7fd95446bc18a969a743398a536f26b2a2d4e71e
ivanaugusto86 <iaaleksink@gmail.com>
2020-08-15 17:12:45 -0300

    importacion de modulos a index.js

--- REDLIFE/functions/index.js
+++ REDLIFE/functions/index.js
@@ -7,3 +7,9 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
