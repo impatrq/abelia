9d51de22c13afca4d1faa5766a40ab7e38b3be38
Andrea Belen Enricci Louzan <abenriccilouzan@gmail.comgit config --global user.name>
2020-09-08 09:55:54 -0300

    functions

--- servidor/functions/index.js
+++ servidor/functions/index.js
@@ -1,15 +1,9 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-
-const serviceAccount = require('./keysdk.json');
-
-
-// The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
-
+const functions = require('firebase-functions');
+const serviceAccount = require('./keysdk.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://abelia.firebaseio.com"
+    databaseURL: "https://abelia.firebaseio.com/"
 });
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
@@ -20,7 +14,7 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
   // Push the new message into Cloud Firestore using the Firebase Admin SDK.
   const writeResult = await admin.firestore().collection('messages').add({original: original});
   // Send back a message that we've succesfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
+  res.json({result: 'Message with ID: ${writeResult.id} added.'});
 });
 
 // Listens for new messages added to /messages/:documentId/original and creates an
@@ -30,7 +24,7 @@ exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
       // Grab the current value of what was written to Cloud Firestore.
       const original = snap.data().original;
 
-      // Access the parameter `{documentId}` with `context.params`
+      // Access the parameter {documentId} with context.params
       functions.logger.log('Uppercasing', context.params.documentId, original);
 
       const uppercase = original.toUpperCase();
